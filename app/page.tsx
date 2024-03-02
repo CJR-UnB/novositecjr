ba549a0b90621958029d858bb44847dc1b6803cb
rafaelghiorzi <rafael.ghiorzi@gmail.com>
2024-03-01 23:27:20 -0300

    feat:setup inicial do projeto

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import CustomIcon, { CJR } from "./components/SVGicons";
 {
   /* As cores principais que combinamos no figma já estão gravadas nos arquivos do projeto com os seguintes nomes:
     aquagreen: "#27BD80",
@@ -6,6 +7,7 @@
     navyblue: "#002F67",
     white: "#FFFFFF",
     black: "#000000",
+    green: "#18FF6D",
 o link para o design do site no figma:
 https://www.figma.com/file/hrDGvvGvbkOCQ3se3v4rM5/CJR?type=design&node-id=1004-176&mode=design&t=ODQEH1MjWQWgwdHr-0
 Se precisarem de ajuda com tailwind, vai aqui um cheatsheet para ajudar
@@ -18,19 +20,44 @@ Vai ficar incrível
 export default function Home() {
   return (
     <main className="text-spaceblue">
-      <header className="">
-        <button>Início</button>
-        <button>Quem somos</button>
-        <button>pipipopo tem que adicionar</button>
+      <header className="justify-between bg-spaceblue text-gray-300 flex px-2 relative">
+        <CJR className="scale-125 ml-40 self-center" />
+        <div className="self-center text-2xl mr-24 space-x-40">
+          <button className="group transition duration-300">
+            Quem somos
+            <span className="block max-w-0 group-hover:max-w-full transition-all duration-500 h-1 bg-aquagreen"></span>
+          </button>
+          <button className="group transition duration-300">
+            Nossos Cases
+            <span className="block max-w-0 group-hover:max-w-full transition-all duration-500 h-1 bg-aquagreen"></span>
+          </button>
+          <button className="group transition duration-300">
+            Serviços e Produtos
+            <span className="block max-w-0 group-hover:max-w-full transition-all duration-500 h-1 bg-aquagreen"></span>
+          </button>
+          <button className="bg-aquagreen py-7 px-20 text-black transition-colors duration-300 hover:bg-green ease-in">
+            Contato
+          </button>
+        </div>
       </header>
-      <section className="" id="Início">
-        <h1 className="text-offwhite">
-          Desenvolvendo ideias e impulsionando resultados
-        </h1>
-        <h2 className="text-offwhite">
-          A CJR tem a solução certa para digitalizar sua empresa e escalar seus
-          serviços
-        </h2>
+      <section
+        className="bg-spaceblue bg-contain text-white w-auto p-2 flex relative"
+        id="Início"
+        style={{ overflow: "hidden" }}
+      >
+        <div className="self-center ml-40">
+          <h1 className="text-5xl w-full max-w-3xl mb-3 leading-snug">
+            Desenvolvendo ideias e impulsionando resultados
+          </h1>
+          <p className="text-gray-200 text-xl mb-8 w-full max-w-md">
+            A CJR tem a solução certa para digitalizar sua empresa e escalar
+            seus serviços
+          </p>
+          <button className="bg-aquagreen rounded-xl py-3 px-16 text-2xl text-black transition-colors duration-300 hover:bg-green ease-linear">
+            Faça um orçamento grátis
+          </button>
+        </div>
+        <CustomIcon className="shrink-0 scale-125 overflow-hidden ml-auto mr-44 mt-32" />
       </section>
       {/* Aqui pode ter aquele coutdown de quantos projetos, anos de funcionamento, membros e nota de satisfação*/}
       <section className="" id="quem somos">
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,7 @@ const config: Config = {
         navyblue: "#002F67",
         white: "#FFFFFF",
         black: "#000000",
+        green: "#18FF6D",
       },
     },
   },
