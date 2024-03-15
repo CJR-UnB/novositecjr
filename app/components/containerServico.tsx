aaa78d9d5caf2c5a040e083ce5c5e1f96cf5b94a
Luccambc <luccamagalhaes@gmail.com>
2024-03-15 11:54:01 -0300

    alterações nos cards de servicos

--- app/components/containerServico.tsx
+++ app/components/containerServico.tsx
@@ -9,7 +9,7 @@ export default function ServiceBox({
 }) {
   return (
     <>
-      <div className="flex flex-col py-2 px-2 items-center justify-center border-solid border-2 border-navyblue rounded h-32 md:max-w-96 md:h-auto">
+      <div className="flex flex-col py-2 px-2 items-center justify-center border-none border-2  bg-slate-50 rounded h-32 md:max-w-96 md:h-72">
         <div className="items-center justify-center">
           <i className="flex items-center justify-center size-10 md:size-16">
             {icone}
@@ -27,6 +27,9 @@ export default function ServiceBox({
             {descricao}
           </p>
         </div>
+        <p className="text-base text-green cursor-pointer hover:underline hover:text-aquagreen hover:duration-500 transition-all md:place-self-start md:pl-8 md:pb-3 ">
+          Saiba mais!
+        </p>
       </div>
     </>
   );
