889876ce0b58ac5bf17b6b705b3bf750b55c4ef0
rafaelghiorzi <rafael.ghiorzi@gmail.com>
2024-05-23 10:25:55 -0300

    testando ads

--- app/pages/_document.js
+++ app/pages/_document.js
@@ -16,8 +16,16 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
             rel="stylesheet"
           />
+              <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-K36HJPK5');</script>
         </Head>
         <body>
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K36HJPK5"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+
           <Main />
           <NextScript />
         </body>
