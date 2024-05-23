3ff733ffc58f80ec408bbb7590173cd4d6686bcc
rafaelghiorzi <rafael.ghiorzi@gmail.com>
2024-05-23 10:38:11 -0300

    feat: implementação do google ads no código

--- app/pages/_document.js
+++ app/pages/_document.js
@@ -5,6 +5,18 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+        <script>
+(function(window, document, script, layerName, id){
+    window[layerName] = window[layerName] || [];
+    window[layerName].push({'gtm.start': new Date().getTime(), event: 'gtm.js'});
+    var f = document.getElementsByTagName(script)[0],
+        j = document.createElement(script),
+        dl = layerName!== 'dataLayer'? '&l=' + layerName : '';
+    j.async = true;
+    j.src = 'https://www.googletagmanager.com/gtm.js?id=' + id + dl;
+    f.parentNode.insertBefore(j, f);
+})(window, document, 'script', 'dataLayer', 'GTM-K36HJPK5');
+</script>
           <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
           <link
             href="https://fonts.googleapis.com/css2?family=Lora:wght@400;700&display=swap"
@@ -16,11 +28,7 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
             rel="stylesheet"
           />
-              <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-K36HJPK5');</script>
+
         </Head>
         <body>
 <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K36HJPK5"
