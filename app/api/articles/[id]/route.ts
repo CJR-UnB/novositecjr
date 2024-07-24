2558a89c7292d16744793ae51c637b146e860c93
rafaelghiorzi <rafael.ghiorzi@gmail.com>
2024-07-24 19:54:38 -0300

    fix: bug DA API

--- app/api/articles/[id]/route.ts
+++ app/api/articles/[id]/route.ts
@@ -12,14 +12,16 @@ export async function GET(
       where: { id: Number(id) },
     });
     if (!article) {
-      return (
-        NextResponse.json({ error: "Artigo não encontrado" }), { status: 404 }
+      return NextResponse.json(
+        { error: "Artigo não encontrado" },
+        { status: 404 }
       );
     }
     return NextResponse.json(article);
   } catch (error) {
-    return (
-      NextResponse.json({ error: "Erro ao buscar artigos" }), { status: 500 }
+    return NextResponse.json(
+      { error: "Erro ao buscar artigos" },
+      { status: 500 }
     );
   }
 }
@@ -33,13 +35,13 @@ export async function DELETE(
     await prisma.article.delete({
       where: { id: Number(id) },
     });
-    return (
-      NextResponse.json({ message: "Artigo deletado com sucesso" }),
+    return NextResponse.json(
+      { message: "Artigo deletado com sucesso" },
       { status: 200 }
     );
   } catch (error) {
-    return (
-      NextResponse.json({ error: "Erro ao deletar artigo PORRA" }),
+    return NextResponse.json(
+      { error: "Erro ao deletar artigo" },
       { status: 500 }
     );
   }
