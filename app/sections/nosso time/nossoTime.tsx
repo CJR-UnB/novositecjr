8a8ab2dda6696a2c484d7dc499d4283493d70b93
Rafael Ghiorzi <rafael.ghiorzi@gmail.com>
2025-05-20 18:48:31 -0300

    Update nossoTime.tsx

--- app/sections/nosso time/nossoTime.tsx
+++ app/sections/nosso time/nossoTime.tsx
@@ -12,48 +12,48 @@ const NossoTime: React.FC = () => {
         {/* a b c d e f g h i j k l m n o p q r s t u v w x y z */}
         <div className="flex flex-wrap justify-around items-center mb-20 gap-20">
           <Card
-            imageSrc="/leandro.JPG"
-            nome="Leandro Kornelius"
+            imageSrc=""
+            nome="Marcos Neres"
             cargo="Diretor de Talentos"
             linkedin="https://www.linkedin.com/in/leandrokornelius/"
             github="https://github.com/LeandroKornelius"
             backgroundSrc="background.png"
           />
           <Card
-            imageSrc="/luiz.JPG"
-            nome="Luiz Augusto Araújo"
+            imageSrc=""
+            nome="José Wilson"
             cargo="Diretor Comercial"
             linkedin="https://www.linkedin.com/in/luiz-augusto-araújo-da-silva-09a937213/"
             github="https://github.com/LuizAugus10"
             backgroundSrc="background.png"
           />
           <Card
-            imageSrc="/luiza.JPG"
-            nome="Luiza dos Santos Costa"
+            imageSrc=""
+            nome="Sarah Novais"
             cargo="Diretora de Projetos"
             linkedin="https://www.linkedin.com/in/luiza-dos-santos-costa-b022a228a/"
             github="https://github.com/luizadsantos"
             backgroundSrc="background.png"
           />
           <Card
-            imageSrc="/pejao.JPG"
-            nome="Pedro João Reffatti"
+            imageSrc=""
+            nome="Luiza Santos"
             cargo="Presidente"
             linkedin="https://www.linkedin.com/in/pedro-joão-reffatti-pinheiro-b3b77b2aa/"
             github="https://github.com/pedrojoaorp"
             backgroundSrc="background.png"
           />
           <Card
-            imageSrc="/rafik.JPG"
-            nome="Rodrigo Rafik"
+            imageSrc=""
+            nome="Letícia Bonfim"
             cargo="Diretor de Publicidade"
             linkedin="https://www.linkedin.com/in/rodrigorafik/"
             github="https://github.com/maetsuji"
             backgroundSrc="background.png"
           />
           <Card
-            imageSrc="/victoria.JPG"
-            nome="Victória Rocha"
+            imageSrc=""
+            nome="Eduardo Barbosa"
             cargo="Diretora de Organização Empresarial"
             linkedin="https://www.linkedin.com/in/victória-rocha-191a9524b/"
             github="https://github.com/rochavictoria"
