f06ab860683973db9a317234ccc41f684cb9c682
aadriellyy <limaadrielly214@gmail.com>
2026-05-03 14:59:17 -0300

    :sparkles: fix: fotos dos diretores e time

--- app/sections/nosso time/nossoTime.tsx
+++ app/sections/nosso time/nossoTime.tsx
@@ -12,52 +12,44 @@ const NossoTime: React.FC = () => {
         {/* a b c d e f g h i j k l m n o p q r s t u v w x y z */}
         <div className="flex flex-wrap justify-around items-center mb-20 gap-20">
         <Card
-            imageSrc="luiza.JPG"
-            nome="Luiza Santos"
+            imageSrc="halycia.png"
+            nome="Halycia de Oliveira"
             cargo="Presidente"
-            linkedin="https://www.linkedin.com/in/luiza-dos-santos-costa-b022a228a/"
-            github="https://github.com/luizadsantos"
+            linkedin="https://www.linkedin.com/in/halycia-de-oliveira-fonseca-111513278/"
+            github="https://github.com/halycia"
             backgroundSrc="background.png"
           />
           <Card
-            imageSrc="/marcos.jpg"
-            nome="Marcos Neres"
-            cargo="Diretor de Talentos"
-            linkedin="https://linkedin.com/in/marcosalexsilvan"
-            github="https://github.com/Marcolino5"
+            imageSrc="giovana.png"
+            nome="Giovana Barbosa"
+            cargo="Diretora de Talentos e Tesouraria"
+            linkedin="https://www.linkedin.com/in/giovana-barbosa-1b54991a3/"
+            github="https://github.com/gio221"
             backgroundSrc="background.png"
           />
           <Card
-            imageSrc="will.jpg"
-            nome="José Wilson"
-            cargo="Diretor Comercial"
-            linkedin="https://www.linkedin.com/in/wilsonsousajúnior/"
-            github="https://github.com/WilsonSousajr"
+            imageSrc="pedro.png"
+            nome="Pedro Americo"
+            cargo="Gerente Comercial"
+            linkedin="https://www.linkedin.com/in/dev-pedro-americo/"
+            github="https://github.com/dev-americo"
             backgroundSrc="background.png"
           />
           <Card
-            imageSrc="sarah.jpg"
-            nome="Sarah Novais"
+            imageSrc="adrielly.png"
+            nome="Adrielly Lima"
             cargo="Diretora de Projetos"
-            linkedin="https://www.linkedin.com/in/sarahnovais/"
-            github="https://github.com/sarahnovaiss"
+            linkedin="www.linkedin.com/in/adrielly-limaa"
+            github="https://github.com/aadriellyy"
             backgroundSrc="background.png"
           />
 
           <Card
-            imageSrc="leticia.jpg"
-            nome="Letícia Bonfim"
-            cargo="Diretora de Publicidade"
-            linkedin="https://www.linkedin.com/in/leticia-bomfim-2b6b93316/"
-            github="https://github.com/LetiBomfim"
-            backgroundSrc="background.png"
-          />
-          <Card
-            imageSrc="dudu.jpg"
-            nome="Eduardo Barbosa"
-            cargo="Diretor de Organização Empresarial"
-            linkedin=""
-            github="https://github.com/EduBarb0sa"
+            imageSrc="joao.png"
+            nome="João Moreira"
+            cargo="Diretor de Imagem e Publicidade"
+            linkedin="https://www.linkedin.com/in/joao-felix-moreira-2a006b2a8/"
+            github="https://github.com/joaofmoreiraa"
             backgroundSrc="background.png"
           />
         </div>
